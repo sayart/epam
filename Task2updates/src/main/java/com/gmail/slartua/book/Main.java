00fc83d17ea2d3e721df1c9d0283aa425c3a3037
Artem Slobodyanyuk <slartua@gmail.com>
2018-10-26 13:59:11 +0300

    add task2 update to maven

---
+++ Task2updates/src/main/java/com/gmail/slartua/book/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.slartua.book;
+
+import com.gmail.slartua.book.controller.Controller;
+
+public class Main {
+	public static void main(String[] args) {
+        new Controller().run();
+    }
+
+}
